e564168776edac8a9089e36aa5543d164d778a28
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2022-08-13 19:08:07 +0200

    Add helping classes for commands and property changes
    __Notes on `NotifyPropertyChangedImpl`:__
    
    The class `NotifyPropertyChangedImpl` implements the interface `INotifyPropertyChanged` which is necessary for the View to notice changes made in the ViewModel.
    
    With this base class it is easier for the ViewModel classes to raise such PropertyChanged events without repeating the necessary boilerplate code each time.
    
    __Notes on `DelegateCommand`:__
    
    The class `DelegateCommand` implements the interface `ICommand` which is necessary for the View to execute commands when e.g. clicking on a GUI element.
    
    With this class, the ViewModel has only to provide the (parameterless) function to execute when calling the command. Optionally, you can also provide a function for checking if the command can be executed.
    
    Using the `DelegateCommand` class, you do not need to implement the full `ICommand` interface for each commands but can focus on the actual execute methods.

---
+++ ViewModel/Command/DelegateCommand.cs
@@ -0,0 +1,81 @@
+using System.Windows.Input;
+
+namespace ViewModel.Command;
+
+/// <summary>
+/// Can be used for parameterless commands by providing the method to execute and an method for checking if the
+/// command currently can be executed.
+/// </summary>
+public class DelegateCommand : ICommand
+{
+    // ==============
+    // FIELDS
+    // ==============
+
+    #region FIELDS
+
+    public event EventHandler? CanExecuteChanged;
+    
+    private readonly Predicate<object?>? _canExecute;
+    private readonly Action _execute;
+
+    #endregion
+
+    // ==============
+    // INITIALIZATION
+    // ==============
+    
+    #region initialization
+
+    public DelegateCommand(Action execute, Predicate<object?>? canExecute = null)
+    {
+        this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        this._canExecute = canExecute;
+    }
+    
+    #endregion
+
+    // ==============
+    // CAN EXECUTE
+    // ==============
+    
+    #region canExecute
+
+    public bool CanExecute(object? parameter)
+    {
+        // always can be executed
+        if (this._canExecute == null)
+        {
+            return true;
+        }
+        
+        // can only be executed on certain conditions
+        return this._canExecute.Invoke(parameter);
+    }
+
+    /// <summary>
+    /// Raises <see cref="CanExecuteChanged"/> to re-check the condition if the command can be
+    /// executed.
+    /// <para/>
+    /// Should be called each time after any variable of the <see cref="CanExecute"/> check is changed.
+    /// </summary>
+    public void RaiseCanExecuteChanged()
+    {
+        this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+    
+    #endregion
+
+    // ==============
+    // EXECUTE
+    // ==============
+    
+    #region execute
+
+    public void Execute(object? parameter)
+    {
+        this._execute?.Invoke();
+    }
+    
+    #endregion
+}
