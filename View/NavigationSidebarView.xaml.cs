4dcc12ec0cf9d3f279e14a4ed4f825fdfa22d421
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2022-08-16 11:30:41 +0200

    Add GUI for navigation sidebar

---
+++ View/NavigationSidebarView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace View;
+
+public partial class NavigationSidebar : UserControl
+{
+    public NavigationSidebar()
+    {
+        InitializeComponent();
+    }
+}
