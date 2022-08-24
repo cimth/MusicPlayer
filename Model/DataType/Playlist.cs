14e778c167592534b15ab2c6b8491dd9ffb54a14
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2022-08-24 10:58:33 +0200

    Use NotifyPropertyChangedImpl for Playlist
    * The total duration of a playlist can only be propagated to the GUI with the notification
    
    * Now the sorting does not need to clear and re-fill the Songs collection because setting a new collection will notify the GUI

--- Model/DataType/Playlist.cs
+++ Model/DataType/Playlist.cs
@@ -1,28 +1,43 @@
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Common;
 
 namespace Model.DataType;
 
-public class Playlist
+public class Playlist : NotifyPropertyChangedImpl
 {
+    // ==============
+    // FIELDS
+    // ==============
+
+    private string _name;
+    private ObservableCollection<Song> _songs;
+    private TimeSpan _totalDuration;
+
     // ==============
     // PROPERTIES
     // ==============
-    
-    public string Name { get; set; }
-    
-    public ObservableCollection<Song> Songs { get; set; }
+
+    public string Name
+    {
+        get => _name; 
+        set => SetField(ref _name, value);
+    }
+
+    public ObservableCollection<Song> Songs
+    {
+        get => _songs;
+        set
+        {
+            SetField(ref _songs, value);
+            this.UpdateTotalDuration(null, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
 
     public TimeSpan TotalDuration
     {
-        get
-        {
-            TimeSpan totalDuration = TimeSpan.Zero;
-            foreach (var song in this.Songs)
-            {
-                totalDuration = totalDuration.Add(song.Duration);
-            }
-            return totalDuration;
-        }
+        get => _totalDuration;
+        set => SetField(ref _totalDuration, value);
     }
     
     // ==============
@@ -31,20 +46,43 @@ public class Playlist
 
     public Playlist(string name, ObservableCollection<Song> songs)
     {
-        this.Name = name;
-        this.Songs = songs;
+        this._name = name;
+        this._songs = songs;
+        
+        this._songs.CollectionChanged += UpdateTotalDuration;
+        this.UpdateTotalDuration(null, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
     }
     
     public Playlist(Song song)
     {
-        this.Name = song.Title;
-        this.Songs = new ObservableCollection<Song> {song};
+        this._name = song.Title;
+        this._songs = new ObservableCollection<Song> {song};
+        
+        this._songs.CollectionChanged += UpdateTotalDuration;
+        this.UpdateTotalDuration(null, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
     }
 
     public Playlist(string name)
     {
-        this.Name = name;
-        this.Songs = new ObservableCollection<Song>();
+        this._name = name;
+        this._songs = new ObservableCollection<Song>();
+        
+        this._songs.CollectionChanged += UpdateTotalDuration;
+        this.UpdateTotalDuration(null, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+    
+    // ==============
+    // UPDATE TOTAL DURATION
+    // ==============
+    
+    private void UpdateTotalDuration(object? sender, NotifyCollectionChangedEventArgs eventArgs)
+    {
+        TimeSpan totalDuration = TimeSpan.Zero;
+        foreach (var song in this.Songs)
+        {
+            totalDuration = totalDuration.Add(song.Duration);
+        }
+        this.TotalDuration = totalDuration;
     }
     
     // ==============
@@ -53,18 +91,6 @@ public class Playlist
     
     public void SortByTitle()
     {
-        // Sort a copy of the collection
-        ObservableCollection<Song> sorted = new ObservableCollection<Song>(this.Songs);
-        sorted = new ObservableCollection<Song>(sorted.OrderBy(song => song.Title));
-
-        // Clear the current collection and re-add the songs (which are now ordered).
-        // Do not simply assign the new sorted collection because then this model class 
-        // would have to implement INotifyOnPropertyChanged to notify the GUI on changing
-        // the collection which is not desired inside this simple DataType.
-        this.Songs.Clear();
-        foreach (var song in sorted)
-        {
-            this.Songs.Add(song);
-        }
+        this.Songs = new ObservableCollection<Song>(this.Songs.OrderBy(song => song.Title));
     }
 }
