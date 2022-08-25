c3632a6b4c98cf4912b9b6547a1f6b5e1a7a9be7
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2022-08-25 15:45:35 +0200

    Split artists correctly
    __Note:__
    
    Sometimes the ";" separator does not get recognized correctly by TagLib, thus split manually.

--- Model/Service/SongImporter.cs
+++ Model/Service/SongImporter.cs
@@ -6,14 +6,25 @@ public class SongImporter
 {
     public Song Import(string songPath)
     {
+        // Read song file
         var songFile = TagLib.File.Create(songPath);
 
+        // Import straight forward attributes
         string title = songFile.Tag.Title;
         string album = songFile.Tag.Album;
         string[] artists = songFile.Tag.Performers;
         uint trackNumber = songFile.Tag.Track;
         TimeSpan duration = songFile.Properties.Duration;
 
-        return new Song(songPath, title, album, artists, trackNumber, duration);
+        // Sometimes the ";" separator does not get recognized correctly by TagLib, thus split manually
+        List<string> allArtists = new();
+        foreach (var artist in artists)
+        {
+            string[] splitArtists = artist.Split(";", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            allArtists.AddRange(splitArtists);
+        }
+
+        // Return an internal Song object
+        return new Song(songPath, title, album, allArtists.ToArray(), trackNumber, duration);
     }
 }
